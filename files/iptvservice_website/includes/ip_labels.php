0314b380a47b104f067bf9fb42844f2a1956d2e7
dancorrigan1 <dancorrigan1@gmail.com>
2025-08-18 18:42:17 -0500

    Delete files/iptvservice_website directory

--- files/iptvservice_website/includes/ip_labels.php
+++
@@ -1,8 +0,0 @@
-<?php
-// includes/ip_labels.php
-// Friendly names for known public IPs. Edit freely.
-return [
-    '136.244.52.57' => ['name' => 'Dan Home IP'],
-    '99.178.122.31' => ['name' => 'Mom & Tom Home IP'],
-    '149.106.85.61' => ['name' => 'Joey Home IP'],
-];
--- files/iptvservice_website/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-require __DIR__ . '/includes/auth.php';
-auth_bootstrap();
-auth_logout();
-header('Location: login.php');
